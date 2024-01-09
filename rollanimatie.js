9704cc200f571489748a4dc71f6099ea4f776ee3
richard <rsdevries07@gmail.com>
2024-01-09 07:36:13 +0100

    click op dobbelsteen
    je kan nu klikken maar het werkt nog niet helemaal want dan kan je niet meer rollen

--- rollanimatie.js
+++ rollanimatie.js
@@ -1,36 +1,8 @@
-let dobbelsteen = document.querySelectorAll(".dobbelsteen");
 let boxfordice = document.querySelectorAll(".boxforedice");
 let aanHetRollen = false;
 var dobbelsteenGetallen = [];
 
-
-function roll() {
-    if (aanHetRollen) {
-        return;
-    }
-    boxfordice.forEach(box =>{
-        box.style.transition = "none";
-        box.style.transform = "translateY(0%)";
-        void box.offsetWidth;
-        box.style.transition = "";
-    })
-
-    aanHetRollen = true;
-    dobbelsteen.forEach((dobbelsteen, index) => {
-        var randomnmr = Math.floor(Math.random() * 6) + 1; // nmr tussen 1 en 6
-        dobbelsteenGetallen.push(randomnmr);
-        rollanimation(randomnmr, index);
-    });
-    console.log(dobbelsteenGetallen);
-    setTimeout(() => aanHetRollen = false, 2000)
-}
-
-function rollanimation(rndNmr, index) {
-    var gaps = 5 + rndNmr;
-    boxfordice[index].style.transition = `transform 1s`;
-    boxfordice[index].style.transform = `translateY(500px)`;
-    boxfordice[index].style.transform = `translateY(-${500 + (gaps * 4) + rndNmr * 100}px)`;
-}
+generatedicesforroll();
 
 function generatedicesforroll() {
     let iteratie = 13;
@@ -41,6 +13,7 @@ function generatedicesforroll() {
         let dicebox = document.querySelector(`.diceroll${i}`);
         for (let j = 2; j < iteratie; j++) {
             let dice = document.createElement('img');
+            dice.classList.add("dobbelsteen");
             dice.src = `dobbelstenen/dice${dicecount}.png`;
             dicebox.appendChild(dice);
             dicecount++;
@@ -50,18 +23,53 @@ function generatedicesforroll() {
         }
     }
 }
+let dobbelstenen = document.querySelectorAll(".dobbelsteen");
+function roll() {
+    if (aanHetRollen) {
+        return;
+    }
+    boxfordice.forEach(box => {  // reset animatie als je weer opnieuw klikt
+        box.style.transition = "none";
+        box.style.transform = "translateY(0%)";
+        void box.offsetWidth;
+        box.style.transition = "";
+    })
 
-generatedicesforroll();
+    aanHetRollen = true;
+    dobbelstenen.forEach((dobbelstenen, index) => {
+        var randomnmr = Math.floor(Math.random() * 6) + 1; // nmr tussen 1 en 6
+        dobbelsteenGetallen.splice();
+        dobbelsteenGetallen.push(randomnmr);
+        rollanimation(randomnmr, index);
+    });
+    console.log(dobbelsteenGetallen);
+    setTimeout(() => aanHetRollen = false, 2000)
+}
+
+function rollanimation(rndNmr, index) {
+    var gaps = 5 + rndNmr;
 
 
-boxfordice.forEach(box =>{
-    box.addEventListener("click", behouddobbelsteen)
+    boxfordice[index].style.transition = "transform 1s";
+    boxfordice[index].style.transform = `translateY(500px)`;
+    boxfordice[index].style.transform = `translateY(-${500 + (gaps * 4) + rndNmr * 100}px)`;
 
-})
+}
 
-function behouddobbelsteen(){
-    console.log("dbstn");
-    this.style.width = "103px";
-    this.style.height = "103px";
-    this.style.border = "3px solid red";
+dobbelstenen.forEach(box => {
+    box.addEventListener("click", function () {
+        behouddobbelsteen(box);
+    });
+});
+
+function behouddobbelsteen(box) {
+    dobbelstenen = document.querySelectorAll(".dobbelsteen");
+    dobbelstenen.forEach(box => { })
+    if (!box.classList.contains("highlighted")) {
+        box.style.outline = "2px solid red";
+        box.classList.add("highlighted");
+    } else {
+        box.style.outline = "";
+        box.classList.remove("highlighted");
+    }
 }
