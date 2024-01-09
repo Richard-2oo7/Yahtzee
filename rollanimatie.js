05256fcdb817e268bdade1a4dec284f2aa20fe8b
Richard <rsdevries07@gmail.com>
2024-01-09 20:06:29 +0100

    behoud dobbelstenen
    als je op de dobbelstenen klikt blijven ze staan

--- rollanimatie.js
+++ rollanimatie.js
@@ -1,8 +1,14 @@
+document.addEventListener("DOMcontentLoaded", function(){
+    document.body.style.zoom = "75%"
+})
+
+
 let boxfordice = document.querySelectorAll(".boxforedice");
 let aanHetRollen = false;
 var dobbelsteenGetallen = [];
-
 generatedicesforroll();
+var dobbelstenen = document.querySelectorAll(".dobbelsteen");
+
 
 function generatedicesforroll() {
     let iteratie = 13;
@@ -23,39 +29,74 @@ function generatedicesforroll() {
         }
     }
 }
-let dobbelstenen = document.querySelectorAll(".dobbelsteen");
+
+
 function roll() {
+    
     if (aanHetRollen) {
         return;
     }
-    boxfordice.forEach(box => {  // reset animatie als je weer opnieuw klikt
+    
+    boxfordice.forEach(box=> {  // reset dobbelstenen naar 0
+        resetDice(box)
+    })
+
+    boxfordice.forEach((box, index) => {
+        if(isHighlighted(box)){
+            
+        } else{
+            var randomnmr = Math.floor(Math.random() * 6) + 1; // nmr tussen 1 en 6
+            dobbelsteenGetallen[index] = randomnmr;
+        }
+
+        rollanimation(randomnmr, index);
+    });
+    console.log(dobbelsteenGetallen);
+    aanHetRollen = true;
+    setTimeout(() => aanHetRollen = false, 2000)
+}
+
+
+function isHighlighted(box){
+    var childs = box.children;
+    for(let i = 0; i < childs.length; i++){
+        if(childs[i].classList.contains("highlighted")){
+            return true;
+        }
+    }
+}
+
+
+function resetDice(box){
+    if(isHighlighted(box)){
+
+    }   else{
         box.style.transition = "none";
         box.style.transform = "translateY(0%)";
         void box.offsetWidth;
         box.style.transition = "";
-    })
+    }
 
-    aanHetRollen = true;
-    dobbelstenen.forEach((dobbelstenen, index) => {
-        var randomnmr = Math.floor(Math.random() * 6) + 1; // nmr tussen 1 en 6
-        dobbelsteenGetallen.splice();
-        dobbelsteenGetallen.push(randomnmr);
-        rollanimation(randomnmr, index);
-    });
-    console.log(dobbelsteenGetallen);
-    setTimeout(() => aanHetRollen = false, 2000)
 }
 
 function rollanimation(rndNmr, index) {
     var gaps = 5 + rndNmr;
+    if(index > 4){
+        return;
+    }
+    
+   var childs = boxfordice[index].children;
+   for(let i = 0; i < childs.length; i++){
+    if(childs[i].classList.contains("highlighted")){
+        return;
+       }
+   }
 
-
-    boxfordice[index].style.transition = "transform 1s";
     boxfordice[index].style.transform = `translateY(500px)`;
-    boxfordice[index].style.transform = `translateY(-${500 + (gaps * 4) + rndNmr * 100}px)`;
+    boxfordice[index].style.transition = "transform 1s";
+    boxfordice[index].style.transform = `translateY(-${500 + (gaps * 7) + rndNmr * 100}px)`;
 
 }
-
 dobbelstenen.forEach(box => {
     box.addEventListener("click", function () {
         behouddobbelsteen(box);
@@ -65,7 +106,7 @@ dobbelstenen.forEach(box => {
 function behouddobbelsteen(box) {
     dobbelstenen = document.querySelectorAll(".dobbelsteen");
     dobbelstenen.forEach(box => { })
-    if (!box.classList.contains("highlighted")) {
+    if (box.style.outline == "") {
         box.style.outline = "2px solid red";
         box.classList.add("highlighted");
     } else {
---
+++ score.js
@@ -0,0 +1,5 @@
+const dataCell = document.querySelectorAll(".scorecell");
+
+function telDobbelstenen(){
+    console.log("hoi")
+}
